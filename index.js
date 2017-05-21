a835ca2032ff4cb66975da502f07a2250d2a1602
Kennedy Rose <kennedy@kennedyrose.com>
2017-05-21 11:20:34 -0500

    File path

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict'
+// TODO parse file
 require('dotenv').config({ silent: true })
 const Nightmare = require('nightmare')
 require('nightmare-download-manager')(Nightmare)
-//require('nightmare-inline-download')(Nightmare)
 const nightmare = Nightmare({
 	show: true,
 	openDevTools: true,
-	alwaysOnTop: false
+	alwaysOnTop: false,
+	paths: {
+		downloads: __dirname
+	}
 })
 nightmare.on('download', function(state, downloadItem){
-	console.log(state)
-	if(state == 'started'){
+	if(state == 'completed'){
 		console.log(downloadItem)
-		nightmare.emit('download', './', downloadItem)
 	}
 })
 
@@ -28,16 +29,16 @@ nightmare
 	// Secondary sort by issue
 	.select('[name="export[items-sorting][1][field]"]', 'issue')
 	.select('[name="export[items-sorting][1][order]"]', 'desc')
+	// Create file
 	.click('[data-export="prepare"]')
 	.wait(`[href="/${process.env.CLZ_USER_ID}/comics/export/download"]`)
+	// Download file
 	.evaluate(function(selector){
 		var el = document.querySelector(selector)
 		el.removeAttribute('target')
 	}, `[href="/${process.env.CLZ_USER_ID}/comics/export/download"]`)
 	.click(`[href="/${process.env.CLZ_USER_ID}/comics/export/download"]`)
-	//.goto(`https://cloud.collectorz.com/${process.env.CLZ_USER_ID}/comics/export/download`)
 	.waitDownloadsComplete()
-	//.download(`https://cloud.collectorz.com/${process.env.CLZ_USER_ID}/comics/export/download`)
-	//.end()
+	.end()
 	.then(console.log)
 	.catch(console.error)
